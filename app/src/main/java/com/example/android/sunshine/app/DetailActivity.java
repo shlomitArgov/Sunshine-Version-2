e54d52eb4f48e28457b4830d5fabe6477c2906bc
shlomitArgov <shlomit.argov@gmail.com>
2015-11-29 21:01:24 +0200

    Add share option to action bar in the detail action.

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -3,7 +3,10 @@ package com.example.android.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +15,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
+    private static final String EXTRA_FORECAST = "com.example.android.sunshine.app.FORECAST_DETAILS";
+    private static final String SUNSHINE_APP = " #SunshineApp";
+    private static final String LOG_TAG = DetailActivity.class.getSimpleName();
+    private static String mForecastText;
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,20 @@ public class DetailActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem shareItem = (MenuItem) menu.findItem(R.id.share_forecast_action);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        String stringExtra = mForecastText + SUNSHINE_APP;
+        //Prevent remaining on the app used for sharing when the task is cancelled
+        shareIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, stringExtra);
+        shareIntent.setType("text/plain");
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        } else {
+            Log.d(LOG_TAG, "ShareActionProvider is null");
+        }
+
         return true;
     }
 
@@ -53,7 +75,6 @@ public class DetailActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
-        private static final String EXTRA_FORECAST = "com.example.android.sunshine.app.FORECAST_DETAILS";
 
         public PlaceholderFragment() {
         }
@@ -64,7 +85,8 @@ public class DetailActivity extends ActionBarActivity {
 
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
             TextView itemTextView = (TextView)rootView.findViewById(R.id.detail_text_view);
-            itemTextView.setText(getActivity().getIntent().getStringExtra(EXTRA_FORECAST));
+            mForecastText = getActivity().getIntent().getStringExtra(EXTRA_FORECAST);
+            itemTextView.setText(mForecastText);
             return rootView;
         }
     }
